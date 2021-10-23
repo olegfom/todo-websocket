79fede766c18e939c2e8557e1856d292746d3a88
Your Name <you@example.com>
2021-10-23 19:31:06 -0400

    first commit

--- src/main/java/com/apress/todo/repository/ToDoRepository.java
+++ src/main/java/com/apress/todo/repository/ToDoRepository.java
@@ -8,4 +8,4 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource
-public interface ToDoRepository extends JpaRepository<ToDo,String> {}
+public interface ToDoRepository extends CrudRepository<ToDo,String> {}
